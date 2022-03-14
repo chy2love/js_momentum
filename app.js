6e041b4da6e6b9cb6ed0c1a4365d932b76175d9b
chy2love <chlgkduddlek@gmail.com>
2022-03-15 00:20:19 +0900

    start

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const a = 5;
-const b = 2;
-let myName = "Choi"
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"; //string 만 포함하는 변수는 대문자로 하는 관습
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem("username", username);
+}
 
-console.log(a / b);
+loginForm.addEventListener("submit", onLoginSubmit);
