c157516bdd256a1f2e93a0b1a432058bd97fcc39
chy2love <chlgkduddlek@gmail.com>
2022-03-18 23:03:11 +0900

    22.03.18 finish login part

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden"; //string 만 포함하는 변수는 대문자로 하는 관습
+const USERNAME_KEY = "username";
+
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (localStorage.getItem("username") === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
