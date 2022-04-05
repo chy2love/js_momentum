e079cd60f664237e46695e3c5cfb2277687a2b12
chy2love <chlgkduddlek@gmail.com>
2022-04-05 10:23:02 +0900

    finsh top bar

--- js/backgroud.js
+++ js/backgroud.js
@@ -1,6 +1,2 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-bgImage.id = "background-img";
-document.body.appendChild(bgImage);
+let randomNum = Math.floor(Math.random() * 3);
+document.querySelector(".background_img").classList.add("bg" + randomNum);
--- js/weather.js
+++ js/weather.js
@@ -11,13 +11,13 @@ function onGeoOk(position) {
       const city = data.name;
       const weather = data.weather[0].main;
       const temperature = data.main.temp;
-      cityName.innerText = city;
-      currentWeather.innerText = weather;
-      averageTemp.innerText = temperature;
+      cityName.innerText = `위치: ${city}`;
+      currentWeather.innerText = `날씨: ${weather}`;
+      averageTemp.innerText = `평균 온도: ${temperature}ºc`;
     })
   );
 }
 function onGeoError() {
   alert("Can't find you, No weather");
 }
-// navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
